6d71997f3a685335e68450cafcb0004767f1d131
Ash Clarke <github@ashclarke.email>
2021-11-16 23:04:26 +0000

    Adds characters view and route
    - Removes about view

--- galaxy-explorer/src/components/Header.vue
+++ galaxy-explorer/src/components/Header.vue
@@ -3,7 +3,8 @@
     <h1>Galaxy Explorer</h1>
 
     <nav>
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/characters">Characters</router-link>
     </nav>
   </header>
 </template>
---
+++ galaxy-explorer/src/views/Characters.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="explor">
+    <h1>Explore the Galaxy</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Explore extends Vue {}
+</script>
