e9bfa11044ab8704ac52591cc3d36c6b0f6ac721
Ash Clarke <github@ashclarke.email>
2021-11-17 22:38:19 +0000

    Updates jest config to ensure custom settings override the preset correctly

--- galaxy-explorer/jest.config.js
+++ galaxy-explorer/jest.config.js
@@ -1,9 +1,14 @@
-module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+const defaultPreset = require("@vue/cli-plugin-unit-jest/presets/typescript-and-babel/jest-preset");
+
+const config = {
+  ...defaultPreset,
   testMatch: [
     "**/src/**/*.spec.ts",
   ],
   transform: {
+    ...defaultPreset.transform,
     "^.+\\.vue$": "vue-jest",
   },
 };
+
+module.exports = config;
