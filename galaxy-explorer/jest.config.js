fb869ed4973caf3c71cd3c53c234e2c87766d2ed
Ash Clarke <github@ashclarke.email>
2021-11-19 23:17:06 +0000

    Fixes lint

--- galaxy-explorer/jest.config.js
+++ galaxy-explorer/jest.config.js
@@ -1,10 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires -- Disabled as this is not a TS context
 const defaultPreset = require("@vue/cli-plugin-unit-jest/presets/typescript-and-babel/jest-preset");
 
 const config = {
   ...defaultPreset,
-  testMatch: [
-    "**/src/**/*.spec.ts",
-  ],
+  testMatch: ["**/src/**/*.spec.ts"],
   transform: {
     ...defaultPreset.transform,
     "^.+\\.vue$": "vue-jest",
